8a3cce9ac8d97d51645095ca62d8c0bc97a16984
Guilherme <trintaeoitogc@gmail.com>
2020-06-30 23:12:36 -0300

    desafio finalizado

---
+++ desafio/desafio-um/main.go
@@ -0,0 +1,11 @@
+//
+// Para compilar: docker build -t guilhermegiacomosimoes/maratona_full_cycle_desafio_um:latest .
+//
+// -t = nome da imagem que vai ser criada
+// 
+
+package main
+
+func main() {
+	print("Hello full cycle")
+}
--- desafios/go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/callicoder/go-docker-compose
-
-require (
-	github.com/go-redis/redis v6.14.2+incompatible
-	github.com/gorilla/context v1.1.1 // indirect
-	github.com/gorilla/mux v1.6.2
-)
