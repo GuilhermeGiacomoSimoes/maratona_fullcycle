37ff54628fa9b7d52955c42573866f1fed8a4499
Guilherme <trintaeoitogc@gmail.com>
2020-06-30 23:16:09 -0300

    comentário arrumado main.go

--- desafio/desafio-um/main.go
+++ desafio/desafio-um/main.go
@@ -1,7 +1,11 @@
 //
-// Para compilar: docker build -t guilhermegiacomosimoes/maratona_full_cycle_desafio_um:latest .
+// Para criar o container: docker build -t guilhermegiacomosimoes/maratona_full_cycle_desafio_um:latest .
 //
 // -t = nome da imagem que vai ser criada
+//
+
+//
+// Para rodar a imagem: docker run guilhermegiacomosimoes/maratona_full_cycle_desafio_um:latest
 // 
 
 package main
